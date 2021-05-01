7851d17b5f1a319e2deb7b0a5ce5f42933d86a87
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-05-01 19:03:56 -0400

    Add MUI overrides

---
+++ src/styles/material.js
@@ -0,0 +1,11 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const material = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#1976D2",
+    },
+  },
+});
+
+export default material;
