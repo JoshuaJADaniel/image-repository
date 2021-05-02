b349106acd0da18b749ef31b1e97100201b76ef2
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-05-02 14:39:45 -0400

    Add <Action /> component

---
+++ src/components/Action/Action.jsx
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+
+import Tooltip from "@material-ui/core/Tooltip";
+import IconButton from "@material-ui/core/IconButton";
+
+const Action = ({ tooltip, icon, ...other }) => {
+  return (
+    <Tooltip title={tooltip}>
+      <IconButton color="inherit" {...other}>
+        {icon}
+      </IconButton>
+    </Tooltip>
+  );
+};
+
+Action.propTypes = {
+  icon: PropTypes.node.isRequired,
+  tooltip: PropTypes.string.isRequired,
+};
+
+export default Action;
