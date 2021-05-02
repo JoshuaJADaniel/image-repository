37f7acafda36dfc52e431e3af1529bef2d5bd8bb
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-05-02 13:15:22 -0400

    Add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-function App() {
-  return (
-    <h1>Hello world!</h1>
-  );
-}
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+
+import Home from "pages/Home";
+import Profile from "pages/Profile";
+
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/profile">
+        <Profile />
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export default App;
