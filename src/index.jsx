dfd8a9c9131792d80b8caa76936d5c2930619bbf
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-05-04 20:28:14 -0400

    Remove base theme provider

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,10 @@ import ReactDOM from "react-dom";
 import App from "App";
 
 import "styles/main.css";
-import material from "styles/material";
-import { ThemeProvider } from "@material-ui/core/styles";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={material}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
