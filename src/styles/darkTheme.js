f5aa3bf7a37520f1e4ce58f9b68b85bca837e50b
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-05-04 20:12:46 -0400

    Create themes

---
+++ src/styles/darkTheme.js
@@ -0,0 +1,16 @@
+import { grey } from "@material-ui/core/colors";
+
+const material = {
+  palette: {
+    type: "dark",
+    border: grey[800],
+    primary: {
+      main: "#333333",
+    },
+    background: {
+      default: "#212121",
+    },
+  },
+};
+
+export default material;
---
+++ src/styles/lightTheme.js
@@ -0,0 +1,12 @@
+import { grey } from "@material-ui/core/colors";
+
+const material = {
+  palette: {
+    border: grey[300],
+    primary: {
+      main: "#1976D2",
+    },
+  },
+};
+
+export default material;
