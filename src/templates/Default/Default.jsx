8d7e4a61d80d218352a3b7d53708e0736028fc18
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-05-04 20:27:32 -0400

    Pass tests and incorporate theme changes

--- src/templates/Default/Default.jsx
+++ src/templates/Default/Default.jsx
@@ -1,29 +1,42 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
 import Box from "@material-ui/core/Box";
 import Toolbar from "@material-ui/core/Toolbar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { unstable_createMuiStrictModeTheme as createMuiTheme } from "@material-ui/core";
 
 import Header from "components/Header";
 import ActionPost from "components/ActionPost";
 
+import darkTheme from "styles/darkTheme";
+import lightTheme from "styles/lightTheme";
+import { getLocalTheme } from "styles/themeStorage";
+
 const DefaultTemplate = ({ title, children }) => {
+  const [dark, setDark] = useState(getLocalTheme());
+
+  const createTheme = () =>
+    dark ? createMuiTheme(darkTheme) : createMuiTheme(lightTheme);
+
   useEffect(() => {
-    document.title = title || "Image Repository";
+    document.title = title;
   }, [title]);
 
   return (
-    <>
-      <Header />
+    <ThemeProvider theme={createTheme()}>
+      <CssBaseline />
+      <Header dark={dark} setDark={setDark} />
       <Toolbar />
       <Box p={3}>{children}</Box>
       <ActionPost />
-    </>
+    </ThemeProvider>
   );
 };
 
 DefaultTemplate.propTypes = {
-  title: PropTypes.string,
+  title: PropTypes.string.isRequired,
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node,
