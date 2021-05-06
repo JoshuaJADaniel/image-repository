b840adfcd5fe6d7b092d4c5720f15578907d3b00
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-05-06 16:51:23 -0400

    Change positioning back to default and update spacing

--- src/components/AlertUser/AlertUser.styles.jsx
+++ src/components/AlertUser/AlertUser.styles.jsx
@@ -1,9 +1,12 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-const styles = makeStyles({
+const styles = makeStyles((theme) => ({
   notifyRoot: {
-    position: "absolute",
+    top: theme.mixins.toolbar.minHeight + theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      top: theme.mixins.toolbar.minHeight + theme.spacing(4),
+    },
   },
-});
+}));
 
 export default styles;
