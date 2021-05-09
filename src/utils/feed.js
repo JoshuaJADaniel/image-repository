9771d54b9d7986fa53e15557272f7d1459207355
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-05-09 17:13:22 -0400

    Add upload script

--- src/utils/feed.js
+++ src/utils/feed.js
@@ -1,37 +1,21 @@
-import Item from "components/Item";
-import Box from "@material-ui/core/Box";
-import CircularProgress from "@material-ui/core/CircularProgress";
+import axios from "axios";
 
-const appendItems = (e, items, getItems, setItems) => {
-  if (e.currentTarget.isProcessing()) {
-    return;
-  }
+const FEED_BACKEND = "https://joshuajadaniel.com/image-repository/api/feed";
+const UPLOAD_ENDPOINT = `${FEED_BACKEND}/upload.php`;
 
-  const nextGroupKey = (+e.groupKey || 0) + 1;
-  const nextKey = items.length;
-
-  e.startLoading();
-  const newItems = getItems(nextGroupKey, nextKey);
-  setItems([...items, ...newItems]);
+const uploadImage = (callback, image, title, access) => {
+  const form = new FormData();
+  form.append("image", image, image.name);
+  form.append("title", title || "");
+  form.append("access", access || "");
+  form.append("jwt", localStorage.getItem("jwt") || "");
+  axios
+    .post(UPLOAD_ENDPOINT, form, {
+      headers: { "Content-Type": "multipart/form-data" },
+    })
+    .then((res) => {
+      callback(res.data);
+    });
 };
 
-const layoutComplete = (e) => {
-  !e.isLayout && e.endLoading();
-};
-
-const mapItems = (items) =>
-  items.map((item) => <Item data-groupkey={item.groupKey} {...item} />);
-
-const loaderStyles = {
-  left: "50%",
-  display: "none",
-  transform: "translateX(-50%)",
-};
-
-const Loader = (
-  <Box py={4} style={loaderStyles}>
-    <CircularProgress />
-  </Box>
-);
-
-export { appendItems, layoutComplete, mapItems, Loader };
+export { uploadImage };
