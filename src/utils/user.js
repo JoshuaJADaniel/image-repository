5dfcf32af8758c670bb6f2ca871000d6ddb4a86f
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-05-09 14:10:07 -0400

    Update variable naming convention and split Google from regular backend

--- src/utils/user.js
+++ src/utils/user.js
@@ -1,15 +1,18 @@
 import axios from "axios";
 
-const USER_BACKEND = "https://joshuajadaniel.com/image-repository/api";
-const GOOGLE_ENDPOINT = `${USER_BACKEND}/google/auth.php`;
-const VALID_ENDPOINT = `${USER_BACKEND}/user/validate.php`;
-const SIGNUP_ENDPOINT = `${USER_BACKEND}/user/signup.php`;
-const LOGIN_ENDPOINT = `${USER_BACKEND}/user/login.php`;
+const USER_BACKEND = "https://joshuajadaniel.com/image-repository/api/user";
+const GOOGLE_BACKEND = "https://joshuajadaniel.com/image-repository/api/google";
+
+const GOOGLE_ENDPOINT = `${GOOGLE_BACKEND}/auth.php`;
+
+const LOGIN_ENDPOINT = `${USER_BACKEND}/login.php`;
+const SIGNUP_ENDPOINT = `${USER_BACKEND}/signup.php`;
+const VALIDATE_ENDPOINT = `${USER_BACKEND}/validate.php`;
 
 const getLoggedIn = (callback, jwt) => {
   const form = new FormData();
   form.append("jwt", jwt || localStorage.getItem("jwt"));
-  axios.post(VALID_ENDPOINT, form).then((res) => {
+  axios.post(VALIDATE_ENDPOINT, form).then((res) => {
     callback(res.data["valid"]);
   });
 };
