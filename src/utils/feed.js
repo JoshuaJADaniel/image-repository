44f67dba1f3ce5c888eb59447f329d095492a5da
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-05-09 19:57:58 -0400

    Add public item grabbing from server

--- src/utils/feed.js
+++ src/utils/feed.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+import ActionLink from "components/ActionLink";
+import OpenInNewIcon from "@material-ui/icons/OpenInNew";
+
 const FEED_BACKEND = "https://joshuajadaniel.com/image-repository/api/feed";
 const UPLOAD_ENDPOINT = `${FEED_BACKEND}/upload.php`;
+const PUBLIC_ENDPOINT = `${FEED_BACKEND}/recent.php`;
 
 const uploadImage = (callback, image, title, access) => {
   const form = new FormData();
@@ -18,4 +22,48 @@ const uploadImage = (callback, image, title, access) => {
     });
 };
 
-export { uploadImage };
+const getPublicItems = (callback, nextGroupKey, nextKey) => {
+  axios.get(`${PUBLIC_ENDPOINT}?page=${nextGroupKey}`).then(({ data }) => {
+    if (!data || !data.length) {
+      callback([]);
+      return;
+    }
+
+    const nextItems = [];
+    for (let i = 0; i < data.length; i++) {
+      const key = nextKey + i;
+      const { url, title, bytes, dimensions } = data[i];
+      let imageSize = "";
+      if (bytes < 1000) {
+        imageSize = `${bytes}b`;
+      } else if (bytes < 1000000) {
+        imageSize = `${Math.round((bytes / 1000) * 100) / 100}kb`;
+      } else {
+        imageSize = `${Math.round((bytes / 1000000) * 100) / 100}mb`;
+      }
+
+      nextItems.push({
+        key: `public-${key}`,
+        size: imageSize,
+        dimensions: dimensions,
+        groupKey: nextGroupKey,
+        title: title,
+        url: url,
+        buttons: [
+          <ActionLink
+            external
+            size="small"
+            tooltip="Open Full Size"
+            icon={<OpenInNewIcon />}
+            key={`home-feed-full-size-${key}`}
+            link={url}
+          />,
+        ],
+      });
+    }
+
+    callback(nextItems);
+  });
+};
+
+export { uploadImage, getPublicItems };
