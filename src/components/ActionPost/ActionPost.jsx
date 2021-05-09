3dad2e9bbf00574391a647de37938dc3d8c4c6f3
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-05-09 17:13:37 -0400

    Add upload modal for user

--- src/components/ActionPost/ActionPost.jsx
+++ src/components/ActionPost/ActionPost.jsx
@@ -1,27 +1,36 @@
+import { useState } from "react";
 import Fab from "@material-ui/core/Fab";
 import AddIcon from "@material-ui/icons/Add";
 import Tooltip from "@material-ui/core/Tooltip";
 
 import useStyles from "./ActionPost.styles";
+import ModalUpload from "../ModalUpload";
 
 const ActionPost = () => {
   const classes = useStyles();
 
+  const [openUpload, setOpenUpload] = useState(false);
+
   const handleUpload = () => {
-    console.log("Upload");
+    setOpenUpload(true);
   };
 
+  const closeOpenUpload = () => setOpenUpload(false);
+
   return (
-    <Tooltip title="Upload Image(s)" placement="left">
-      <Fab
-        color="secondary"
-        aria-label="upload"
-        onClick={handleUpload}
-        className={classes.fab}
-      >
-        <AddIcon />
-      </Fab>
-    </Tooltip>
+    <>
+      <ModalUpload open={openUpload} onClose={closeOpenUpload} />
+      <Tooltip title="Upload Image" placement="left">
+        <Fab
+          color="secondary"
+          aria-label="upload"
+          onClick={handleUpload}
+          className={classes.fab}
+        >
+          <AddIcon />
+        </Fab>
+      </Tooltip>
+    </>
   );
 };
 
